5ce6359ece05077f8020dd2640f1f7c6fd602264
Noah Lidell <noahlidell@gmail.com>
2018-03-18 15:26:04 -0500

    some exercises

--- epi_judge_python/closest_int_same_weight.py
+++ epi_judge_python/closest_int_same_weight.py
@@ -1,6 +1,10 @@
 def closest_int_same_bit_count(x):
     # Implement this placeholder.
-    return 0
+    for i in range(64-1):
+        if (x >> i & 1) != (x >> (i+1) & 1):
+            x^= 1 << i
+            x ^= 1 << i+1
+            return x
 
 
 from test_framework import test_utils_generic_main, test_utils
