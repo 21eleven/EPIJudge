732239984b821d3d56f271413cd954fb1b486d54
Noah Lidell <noahlidell@gmail.com>
2018-03-25 16:10:59 -0500

    multiply using primitives in c++

--- epi_judge_cpp/primitive_multiply.cc
+++ epi_judge_cpp/primitive_multiply.cc
@@ -1,7 +1,47 @@
+unsigned long Add(unsigned long a, unsigned long b) {
+	short int carry = 0;
+	unsigned long sum = 0;
+	unsigned long place = 1;
+
+	while ((a > 0) || (b > 0) || (carry > 0)) {
+		if ((a & 1) && (b & 1)) {
+			if (carry == 1) {
+				sum ^=  place;
+			} else {
+				carry = 1;
+			}
+		} else if ((a & 1) || (b & 1)) {
+			if (carry == 0) {
+				sum ^= place;
+			}
+		} else {
+			if (carry == 1) {
+				sum ^= place;
+				carry = 0;
+			}
+		}
+		place <<= 1;
+		a >>= 1;
+		b >>= 1;
+	}
+	return sum;
+}
+		
+				
+
+
 
 unsigned long Multiply(unsigned long x, unsigned long y) {
   // Implement this placeholder.
-  return 0;
+  unsigned long product = 0;
+  while (x) {
+	  if (x & 1) {
+		 product = Add(product, y);
+	  }
+  	  y <<= 1;
+	  x >>= 1;
+  }  
+  return product;
 }
 
 #include "test_framework/test_utils_generic_main.h"
