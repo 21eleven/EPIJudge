b2bedc04ad8eabdaab153c5894969f29836a81d1
Noah Lidell <noahlidell@gmail.com>
2018-03-26 13:02:50 -0500

    java count bits, divide, and multiply. also added a file for tracking progress.

--- epi_judge_java/epi/PrimitiveDivide.java
+++ epi_judge_java/epi/PrimitiveDivide.java
@@ -7,7 +7,16 @@ public class PrimitiveDivide {
   @EpiTest(testfile = "primitive_divide.tsv")
   public static int divide(int x, int y) {
     // Implement this placeholder.
-    return 0;
+    short power = 32;
+    int result = 0;
+    while (x >= y) {
+    	while (1 << power > x) {
+	  power -= 1;
+	}
+	result += 1 << power;
+	x -=  y << power;
+    }
+    return result;
   }
 
   public static void main(String[] args) {
--- epi_judge_java/epi/PrimitiveMultiply.java
+++ epi_judge_java/epi/PrimitiveMultiply.java
@@ -7,7 +7,43 @@ public class PrimitiveMultiply {
   @EpiTest(testfile = "primitive_multiply.tsv")
   public static long multiply(long x, long y) {
     // Implement this placeholder.
-    return 0;
+    long product = 0;
+    while (x > 0 ) {
+    	if (( x & 1)==1) {
+		product = add(product,y);
+	}
+	y <<= 1;
+	x >>= 1;
+    }
+    return product;
+  }
+
+  public static long add(long a, long b) {
+    short carry = 0;
+    long sum = 0;
+    long place = 1;
+    while ((a > 0) || (b > 0) || (carry >0)) {
+	    if (((a & 1)==1) && ((b & 1)==1)) {
+	      if (carry == 0) {
+		carry = 1;
+	      } else {
+	        sum ^= place;
+	      }
+	    } else if (((a & 1)==1) || ((b & 1)==1)) {
+	      if (carry == 0) {
+		sum ^= place;
+	      } 
+	    } else {
+	      if (carry == 1) {
+		sum ^= place;
+		carry = 0;
+	      }
+	    }
+	    a >>= 1;
+	    b >>= 1;
+	    place <<= 1;
+    }
+    return sum;
   }
 
   public static void main(String[] args) {
