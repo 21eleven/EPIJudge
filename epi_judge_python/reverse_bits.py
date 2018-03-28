c6ca484170203406f59de70eeaeed740a8ff6d44
Noah Lidell <noahlidell@gmail.com>
2018-03-27 17:48:10 -0700

    bit reversal in py3

--- epi_judge_python/reverse_bits.py
+++ epi_judge_python/reverse_bits.py
@@ -1,7 +1,24 @@
 def reverse_bits(x):
     # Implement this placeholder.
-    return 0
+    #lookup = init_look_up()
+    return (lookup[x&0xFFFF] << 48)^(lookup[x>>16&0xFFFF] << 32)^(lookup[x>>32&0xFFFF] << 16)^(lookup[x>>48]) 
 
+def init_look_up():
+    d = {}
+    for x in range(0xFFFF+1):
+       rev = x
+       for i in range(8):
+          rev = swap(rev,0+i,15-i)
+       d[x]=rev
+    return d
+
+
+def swap(x,i,j):
+    if (x>>i)&1 != (x>>j)&1:
+       x ^= 1<<i
+       x ^= 1<<j
+    return x
+lookup = init_look_up()
 
 from test_framework import test_utils_generic_main, test_utils
 
