6e303044b0be74265211c94e7ae1be571de57bc6
Noah Lidell <noahlidell@gmail.com>
2018-04-19 14:46:27 -0700

    digit reversal and number palindrome checking in py3

--- epi_judge_python/reverse_digits.py
+++ epi_judge_python/reverse_digits.py
@@ -1,7 +1,17 @@
 def reverse(x):
-    # Implement this placeholder.
-    return 0
-
+	# Implement this placeholder.
+	result = 0
+	neg = False 
+	if x < 0:
+		neg = True
+		x *= -1
+	while x:
+		result = result * 10 + x % 10
+		x//= 10
+	if neg == True:
+		result *= -1
+	return result
+	
 
 from test_framework import test_utils_generic_main, test_utils
 
