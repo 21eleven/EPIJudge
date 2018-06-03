0562c87bf350c6165b3e7a1ce9250d4f0ab57c5a
Noah Lidell <noahlidell@gmail.com>
2018-06-03 00:54:40 -0500

    skeleton.rs fix

--- epi_judge_rust/skeleton.rs
+++ epi_judge_rust/skeleton.rs
@@ -2,7 +2,7 @@ use std::env;
 
 fn main() {
 	let args: Vec<String> = env::args().collect();
-	let input = &args[1].parse::<u64>().unwrap();
+	let input = args[1].parse::<u64>().unwrap();
 
 	println!("{}", output);
 }
